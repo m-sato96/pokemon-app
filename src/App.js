d98461e55a9bc3e19038b171695899364698df1d
m-sato <ssmmaagg36@gmail.com>
2023-02-14 19:47:22 +0900

    ページャー実装

--- src/App.js
+++ src/App.js
@@ -6,22 +6,32 @@ function App() {
   const initialURL = "https://pokeapi.co/api/v2/pokemon";
   const [loading, setLoading] = useState(true);
   const [pokemonData, setPokemonData] = useState([]);
+  const [nextPage, setNextPage] = useState("");
+  const [prevPage, setPrevPage] = useState("");
+
+  const fetchData = async (url) => {
+    setLoading(true);
+    const response = await fetch(url);
+    const data = await response.json();
+    setPrevPage(data.previous);
+    setNextPage(data.next);
+    const urls = data.results.map((result) => result.url);
+    const _pokemonData = await Promise.all(
+      urls.map(async (url) => {
+        const pokemonResponse = await fetch(url);
+        return pokemonResponse.json();
+      })
+    );
+    setPokemonData(_pokemonData);
+    setLoading(false);
+  };
+
   useEffect(() => {
-    const fetchData = async () => {
-      const response = await fetch(initialURL);
-      const data = await response.json();
-      const urls = data.results.map((result) => result.url);
-      const _pokemonData = await Promise.all(
-        urls.map(async (url) => {
-          const pokemonResponse = await fetch(url);
-          return pokemonResponse.json();
-        })
-      );
-      setPokemonData(_pokemonData);
-      setLoading(false);
-    };
-    fetchData();
+    fetchData(initialURL);
   }, []);
+
+  const noPrevious = prevPage === null ? "disabled" : "";
+  const noNext = nextPage === null ? "disabled" : "";
 
   return (
     <div className="App">
@@ -35,6 +45,24 @@ function App() {
               <Card key={pokemon.id} pokemon={pokemon}></Card>
             ))}
           </div>
+          <ul className="pager">
+            <li
+              className={noPrevious}
+              onClick={() => {
+                fetchData(prevPage);
+              }}
+            >
+              前へ
+            </li>
+            <li
+              className={noNext}
+              onClick={() => {
+                fetchData(nextPage);
+              }}
+            >
+              次へ
+            </li>
+          </ul>
         </div>
       )}
     </div>
