132a60f7b092c4b0c5f5567b9b69fa2093022e26
m-sato <ssmmaagg36@gmail.com>
2023-02-14 16:29:09 +0900

    ポケモンデータ取得と表示

--- src/App.js
+++ src/App.js
@@ -1,7 +1,44 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  const initialURL = "https://pokeapi.co/api/v2/pokemon";
+  const [loading, setLoading] = useState(true);
+  const [pokemonData, setPokemonData] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(initialURL);
+      const data = await response.json();
+      const urls = data.results.map((result) => result.url);
+      const _pokemonData = await Promise.all(
+        urls.map(async (url) => {
+          const pokemonResponse = await fetch(url);
+          return pokemonResponse.json();
+        })
+      );
+      setPokemonData(_pokemonData);
+      setLoading(false);
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div className="App">
+      {loading ? (
+        <p>読み込み中...</p>
+      ) : (
+        <div>
+          <h1>Pokemon List</h1>
+          {pokemonData.map((pokemon) => (
+            <div key={pokemon.id}>
+              <h2>{pokemon.name}</h2>
+              <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
